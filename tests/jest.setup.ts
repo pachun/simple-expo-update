002ec20a38e307d68f60d76857663dbea2c1d6ce
Nick Pachulski <nick@pachulski.me>
2025-01-29 03:57:01 -0500

    Upgrade to expo SDK 52

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,5 +1,4 @@
 jest.mock("expo-updates", () => ({
-  ...jest.requireActual("expo-updates"),
   checkForUpdateAsync: jest.fn().mockResolvedValue({ isAvailable: false }),
   fetchUpdateAsync: jest.fn().mockResolvedValue({ isNew: false }),
   reloadAsync: jest.fn().mockResolvedValue(undefined),
